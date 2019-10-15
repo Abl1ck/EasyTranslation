6241c962f3c6e69d85ed25b5da05925adfe7bfec
Alvin <alvinmambele@gmail.com>
2019-10-15 10:51:36 +0200

    Create lang-en.php

---
+++ exemples/languages/lang-en.php
@@ -0,0 +1,20 @@
+<?php
+#-------------------------------------------------------------------------------#
+#																				#
+# Interpreteur	: PHP 5															#
+# Lincence		: GPL															#
+# Version		: 1.0.1															#
+# Auteur		: Alvin Mambele													#
+# Pays			: R.D.Congo														#
+# Copyright		: Copyright (C) 2019 Easysoft Cd. Tous droits reservés			#
+# Langue		: English 														#
+#-------------------------------------------------------------------------------#
+
+$EASY_LANG['zero_devid'] 		= 'impossible to divide by zero.';
+$EASY_LANG['user_connect'] 		= 'unable to connect with this user.';
+$EASY_LANG['no_fielad'] 		= 'Invalid field name.';
+$EASY_LANG['no_table'] 			= 'can not find the selected table in the database.';
+$EASY_LANG['connect_host'] 		= 'unable to connect to the server.';
+$EASY_LANG['Welcome'] 			= 'Welcome to EasyTranslation';
+
+?>
