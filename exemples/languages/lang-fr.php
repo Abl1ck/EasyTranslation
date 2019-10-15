6552f83d530d51145b6a9592de31feb7bcd06c81
Alvin <alvinmambele@gmail.com>
2019-10-15 10:52:11 +0200

    Create lang-fr.php

---
+++ exemples/languages/lang-fr.php
@@ -0,0 +1,20 @@
+<?php
+#-------------------------------------------------------------------------------#
+#																				#
+# Interpreteur	: PHP 5															#
+# Lincence		: GPL															#
+# Version		: 1.0.1															#
+# Auteur		: Alvin Mambele													#
+# Pays			: R.D.Congo														#
+# Copyright		: Copyright (C) 2019 Easysoft Cd. Tous droits reservés			#
+# Langue		: French (reference file: refer to English translation)			#
+#-------------------------------------------------------------------------------#
+
+$EASY_LANG['zero_devid'] 		= 'impossible de diviser par zéro.';
+$EASY_LANG['user_connect'] 		= 'impossible de se connecter avec cet utilisateur.';
+$EASY_LANG['no_fielad'] 		= 'nom de champ incorrect.';
+$EASY_LANG['no_table'] 			= 'impossible de trouver la table dans la base de données.';
+$EASY_LANG['connect_host'] 		= 'impossible de se connecter au serveur.';
+$EASY_LANG['Welcome'] 			= 'Bienvenue dans EasyTranslation';
+
+?>
